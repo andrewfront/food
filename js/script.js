7cde7fcd7db463a7f36066770227d2587fbb43cd
Andrew <andrew.ndrw1@gmail.com>
2021-06-24 22:20:14 +0300

    sendJSON

--- js/script.js
+++ js/script.js
@@ -82,7 +82,7 @@ document.addEventListener('DOMContentLoaded', () => {
         modal.classList.add('show')
         modal.classList.remove('hide')
         document.body.style.overflow = 'hidden'
-        clearInterval(timerId)
+        // clearInterval(timerId)
     }
     modalTrigger.forEach(btn => {
         btn.addEventListener('click', showModal)
@@ -172,4 +172,44 @@ new menuCard(
     9,
     '.menu .container'
 ).render()
+//отправка данных на сервер
+    const forms = document.querySelectorAll('form')
+    const message = {
+        loading: 'Идет Загрузка',
+        success: 'Ваше Сообщение Отправлено',
+        failure: 'Произошла Ошибка'
+    }
+    forms.forEach(item => {
+        postData(item)
+    })
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault()
+            const statusMessage = document.createElement('div')
+            statusMessage.classList.add('status')
+            statusMessage.textContent = message.loading
+            form.append(statusMessage)
+            const request = new XMLHttpRequest()
+            request.open('POST', 'server.php')
+            request.setRequestHeader('Content-type', 'application/json')
+            const formData = new FormData(form)
+            const object = {}
+            formData.forEach(function(value, key) {
+                object[key] = value
+            })
+            const json = JSON.stringify(object)
+            request.send(json)
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    statusMessage.textContent = message.success
+                    form.reset()
+                    setTimeout(() => {
+                        statusMessage.remove()
+                    }, 2000)
+                } else {
+                    statusMessage.textContent = message.failure
+                }
+            })
+        })
+    }
 })
