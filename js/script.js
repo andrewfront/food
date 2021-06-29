69957a1ec8005e88cfd95178396019ce63164245
Andrew <andrew.ndrw1@gmail.com>
2021-06-29 20:20:09 +0300

    fullslider

--- js/script.js
+++ js/script.js
@@ -236,6 +236,7 @@ document.addEventListener('DOMContentLoaded', () => {
     .then(data => data.json())
     .then(res => console.log(res))
     //slider
+    const slider = document.querySelector('.offer__slider')
     const prevBtn = document.querySelector('.offer__slider-prev')
     const nextBtn = document.querySelector('.offer__slider-next')
     const currentNum = document.querySelector('#current')
@@ -260,6 +261,47 @@ document.addEventListener('DOMContentLoaded', () => {
     slides.forEach(slide => {
         slide.style.width = width
     })
+    slider.style.position = 'relative'
+    const indicators = document.createElement('ol')
+    const dots = []
+    indicators.classList.add('carousel-indicators')
+    indicators.style.cssText = `
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 15;
+    display: flex;
+    justify-content: center;
+    margin-right: 15%;
+    margin-left: 15%;
+    list-style: none;
+    `
+    slider.append(indicators)
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li')
+        dot.setAttribute('data-slide-to', i + 1)
+        dot.style.cssText = `
+        box-sizing: content-box;
+        flex: 0 1 auto;
+        width: 30px;
+        height: 6px;
+        margin-right: 3px;
+        margin-left: 3px;
+        cursor: pointer;
+        background-color: #fff;
+        background-clip: padding-box;
+        border-top: 10px solid transparent;
+        border-bottom: 10px solid transparent;
+        opacity: .5;
+        transition: opacity .6s ease;
+        `
+        if (i == 0) {
+            dot.style.opacity = 1
+        }
+        indicators.append(dot)
+        dots.push(dot)
+    }
     nextBtn.addEventListener('click', () => {
         if (offSet == +width.slice(0, width.length - 2) * (slides.length - 1)) {
             offSet = 0
@@ -277,6 +319,8 @@ document.addEventListener('DOMContentLoaded', () => {
         } else {
             currentNum.textContent = slideIndex
         }
+        dots.forEach(dot => dot.style.opacity = 0.5)
+        dots[slideIndex - 1].style.opacity = 1
     })
     prevBtn.addEventListener('click', () => {
         if (offSet == 0) {
@@ -295,6 +339,23 @@ document.addEventListener('DOMContentLoaded', () => {
         } else {
             currentNum.textContent = slideIndex
         }
+        dots.forEach(dot => dot.style.opacity = 0.5)
+        dots[slideIndex - 1].style.opacity = 1
+    })
+    dots.forEach(dot => {
+        dot.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute('data-slide-to')
+            slideIndex = slideTo
+            offSet = +width.slice(0, width.length - 2) * (slideTo - 1)
+            slidesField.style.transform = `translateX(-${offSet}px)`
+            if (slides.length < 10) {
+                currentNum.textContent = `0${slideIndex}`
+            } else {
+                currentNum.textContent = slideIndex
+            }
+            dots.forEach(dot => dot.style.opacity = 0.5)
+            dots[slideIndex - 1].style.opacity = 1
+        })
     })
     // showSlides(slideIndex)
     // if (slides.length < 10) {
